05e02dda84a4312a3d46d2d8fc58a53edb1e4408
fiote <murilo.eng@gmail.com>
2021-03-27 19:31:00 -0300

    adjusting check for hotkeys

--- Patches/Player_Patch.cs
+++ Patches/Player_Patch.cs
@@ -34,7 +34,7 @@ namespace RunicPower.Patches {
 	[HarmonyPatch(typeof(Player), "UseHotbarItem")]
 	public static class Player_UseHotbarItem_Patch {
 		public static bool Prefix(Player __instance, int index) {
-			var item = SpellsBar.GetSpellHotKeyItem(__instance, index, true);
+			var item = SpellsBar.GetSpellHotKeyItem(__instance, index-1, true);
 			return (item == null);
 		}
 	}
