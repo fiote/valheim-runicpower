7d87e6d807aa62abf16b403b9f06d5adb9f590cf
fiote <murilo.eng@gmail.com>
2021-04-18 11:49:05 -0300

    recreating elements when the game's scale change

---
+++ Patches/Menu_Patch.cs
@@ -0,0 +1,20 @@
+﻿using HarmonyLib;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+namespace RunicPower {
+
+	[HarmonyPatch(typeof(GuiScaler), "SetScale")]
+
+	public static class GuiScaler_SetScale_Patch {
+
+		public static float lastScale = -100f;
+
+		public static void Postfix(GuiScaler __instance, float scale) {
+			if (scale == lastScale) return;
+			lastScale = scale;
+			RunicPower.Recreate();
+		}
+	}
+}
