4f086cf9ddabfa2b1d0588568bd2519c8c80ef92
fiote <murilo.eng@gmail.com>
2022-09-23 16:40:01 -0300

    xxx - checking for factionboss as well when doing damage

--- Prototype/Player_Prototype.cs
+++ Prototype/Player_Prototype.cs
@@ -30,7 +30,7 @@ namespace RunicPower.Patches {
 
 		public static bool CanHarmWithRunes(this Player __instance, Character other) {
 			// if the other is a monster of a boss, it CAN be harmed
-			if (other.IsMonsterFaction() || other.IsBoss()) return true;
+			if (other.IsMonsterFaction() || other.m_faction == Character.Faction.Boss || other.IsBoss()) return true;
 			// if the player is not flagged as pvp, it CAN NOT harm others players
 			if (!__instance.IsPVPEnabled()) return false;
 			// if the pvp config is not enabled, it CAN NOT harm others players
